f82fe5e7a6f8ec77128f740a7516497cc9eea020
Cameron <cameronlpmoore@gmail.com>
2017-10-21 23:12:08 +0000

    Variadic print functions (to be completed)

---
+++ print_functions.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdarg.h>
+
+/**
+ * print_int - prints an integer number
+ *
+ *
+ */
+int print_int(va_list arg)
+{
+}
+
+/**
+ * print_float - prints a floating point number
+ *
+ *
+ */
+int print_float(va_list arg)
+{
+}
+
+/**
+ * print_char - prints a character
+ *
+ *
+ */
+int print_char(va_list arg)
+{
+}
+
+/**
+ * print_string - prints a string
+ *
+ *
+ */
+int print_str(va_list arg)
+{
+}
