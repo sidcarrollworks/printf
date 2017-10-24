7b7f2341bcadcfb59388a4942ab206791d743483
Cameron <cameronlpmoore@gmail.com>
2017-10-24 16:54:00 +0000

    Functions for printing numbers

--- helper_funcs.c
+++ helper_funcs.c
@@ -16,3 +16,35 @@ void _printihelp(int n)
 
 	_putchar(n % 10 + '0');
 }
+
+int print_nums(va_list list)
+{
+	int n = va_list(list, int);
+	int len = 0;
+	int div = 1;
+	int num;
+
+	num = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num *= -1;
+		len++;
+	}
+	else
+	{
+		num = n;
+		while ((num / 10) >= 10)
+		{
+			div *= 10;
+		}
+		while (div > 0)
+		{
+			_putchar((num / div) + '0');
+			num %= div;
+			div /= 10;
+			len++;
+		}
+	}
+	return (len);
+}
